b8d6f2a4a36583ff55b3fa217315b68d7e98e13d
dreyyan <adriandominic.tan@wvsu.edu.ph>
2024-09-20 21:21:14 +0800

    Add files via upload

---
+++ employeeSalary.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Employee {
+private:
+    string employeeName;
+    double hourlyRate;
+    int hoursWorked;
+
+
+public:
+    void inputEmployeeDetails() {
+        cout << "Enter Name: ";
+        getline(cin, employeeName);
+        cout << "\nEnter rate/hr.: ";
+        cin >> hourlyRate;
+        cout << "\nEnter total hrs. Worked: ";
+        cin >> hoursWorked;
+    }
+
+    void totalSalary() {
+        double totalSalary = hourlyRate * hoursWorked;
+        cout << "\nYour total salary is: $" << totalSalary << "\n";
+    }
+
+    void displayEmployeeDetails() {
+        cout << "\nEmployee Created Successfully!";
+        cout << "\nEmployee Name: " << employeeName;
+        cout << "\nRate/hour: " << hourlyRate;
+        cout << "\nTotal Hours Worked: " << hoursWorked << "\n";
+    }
+};
+
+int main() {
+    Employee emp1;
+
+    emp1.inputEmployeeDetails();
+    emp1.displayEmployeeDetails();
+    emp1.totalSalary();
+    return 0;
+}
