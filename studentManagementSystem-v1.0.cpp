5528290e48fbe1921328e295c62c7106a790a110
dreyyan <adriandominic.tan@wvsu.edu.ph>
2024-10-19 10:20:15 +0800

    studentManagementSystem-v1.1.cpp

--- studentManagementSystem-v1.0.cpp
+++ studentManagementSystem-v1.0.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <list>
 using namespace std;
 
+// TO IMPROVE:
+// class CourseManager;
+// studentList;
+// instructorList;
+// case 3 & 4
+
 // Person > Student/Instructor
-//
+// Department
+// Course
+// Course Manager
 
 // Base Class
 class Person {
@@ -20,7 +29,6 @@ public:
 class Student : public Person {
 protected:
     string id;
-
 public:
     Student(string studentName, string studentId, char studentGender, string studentDateOfBirth, string studentEmail, string studentAddress, string studentContactNumber)
         : Person(studentName, studentGender, studentDateOfBirth, studentEmail, studentAddress, studentContactNumber), id(studentId) {
@@ -36,7 +44,22 @@ public:
         cout << "\nContact No.: " << contactNumber;
         cout << "\n--------------------------------------------";
     }
+
+    void displayInfo() {
+        cout << "\n--------------------------------------------";
+        cout << "\nSTUDENT INFORMATION |";
+        cout << "\nFull Name: " << name;
+        cout << "\nID: " << id;
+        cout << "\nGender(M/F): " << gender;
+        cout << "\nDate of Birth(MM/DD/YYYY): " << dateOfBirth;
+        cout << "\nEmail Address: " << email;
+        cout << "\nAddress: " << address;
+        cout << "\nContact No.: " << contactNumber;
+        cout << "\n--------------------------------------------";
+    }
 };
+
+list<Student> studentList;
 
 // Derived Class
 class Instructor : public Person {
@@ -56,7 +79,22 @@ public:
         cout << "\nContact No.: " << contactNumber;
         cout << "\n--------------------------------------------";
     }
+
+    void displayInfo() {
+        cout << "\n--------------------------------------------";
+        cout << "\nINSTRUCTOR INFORMATION |";
+        cout << "\nFull Name: " << name;
+        cout << "\nGender(M/F): " << gender;
+        cout << "\nDate of Birth(MM/DD/YYYY): " << dateOfBirth;
+        cout << "\nEmail Address: " << email;
+        cout << "\nAddress: " << address;
+        cout << "\nContact No.: " << contactNumber;
+        cout << "\n--------------------------------------------";
+    }
+
 };
+
+list<Instructor> instructorList;
 
 class Department {
 protected:
@@ -100,14 +138,93 @@ protected:
 public:
 };
 
+void loopMenu() {
+    int choice;
+
+    cout << "\n| STUDENT MANAGEMENT SYSTEM |";
+    cout << "\n[1] | Add Student";
+    cout << "\n[2] | Add Instructor";
+    cout << "\n[3] | Add Department";
+    cout << "\n[4] | Add Course";
+    cout << "\n[5] | Exit";
+    cout << "\nEnter Number: ";
+    cin >> choice;
+
+    switch (choice) {
+        case 1: {
+            string name, id, dateOfBirth, email, address, contactNumber;
+            char gender;
+
+            cin.ignore();
+            cout << "\nSTUDENT INFORMATION |";
+            cout << "\nFull Name: ";
+            getline(cin, name);
+            cout << "\nID: " << id;
+            getline(cin, id);
+            cout << "\nGender(M/F): ";
+            cin >> gender;
+            cin.ignore();
+            cout << "\nDate of Birth(MM/DD/YYYY): ";
+            getline(cin, dateOfBirth);
+            cout << "\nEmail Address: ";
+            getline(cin, email);
+            cout << "\nAddress: ";
+            getline(cin, address);
+            cout << "\nContact No.: ";
+            getline(cin, contactNumber);
+
+            Student student(name, id, gender, dateOfBirth, email, address, contactNumber);
+            ::studentList.push_back(student);
+            student.displayInfo();
+            break;
+        }
+        case 2: {
+            string name, dateOfBirth, email, address, contactNumber;
+            char gender;
+
+            cin.ignore();
+            cout << "\nINSTRUCTOR INFORMATION |";
+            cout << "\nFull Name: ";
+            getline(cin, name);
+            cout << "\nGender(M/F): ";
+            cin >> gender;
+            cin.ignore();
+            cout << "\nDate of Birth(MM/DD/YYYY): ";
+            getline(cin, dateOfBirth);
+            cout << "\nEmail Address: ";
+            getline(cin, email);
+            cout << "\nAddress: ";
+            getline(cin, address);
+            cout << "\nContact No.: ";
+            getline(cin, contactNumber);
+
+            Instructor instructor(name, gender, dateOfBirth, email, address, contactNumber);
+            ::instructorList.push_back(instructor);
+            break;
+        }
+
+        case 3: {
+
+        }
+
+        case 4: {
+
+        }
+
+        case 5:
+            cout << "\nProgram exited successfully.";
+            return;
+        break;
+    }
+}
+
 int main() {
-    /*Student* s1 = new Student("Adrian Dominic L. Tan", "117591120149", 'M', "07/29/2005", "adriandominic.tan@wvsu.edu.ph", "Arevalo, Iloilo City", "09502271058");
-    Instructor* i1 = new Instructor("Walter Hartwell White", 'M', "09/07/1958", "walterhartwell.white@wvsu.edu.ph", "3828 Piermont Dr, Albuquerque, NM.", "555-1258");*/
+    loopMenu();
     Department* d1 = new Department("CICT");
     Course* c1 = new Course(d1, "BSCS");
-    Course *c2 = new Course(d1, "BSIT");
-    Course *c3 = new Course(d1, "BSIS");
-    Course *c4 = new Course(d1, "BLIS");
+    Course* c2 = new Course(d1, "BSIT");
+    Course* c3 = new Course(d1, "BSIS");
+    Course* c4 = new Course(d1, "BLIS");
     d1->printCourseList();
     return 0;
 }
