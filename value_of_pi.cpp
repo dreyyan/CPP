22d47a0e4a8bb277c3e456e195ec6d18b746e0db
dreyyan <adriandominic.tan@wvsu.edu.ph>
2024-11-15 00:53:04 +0800

    Add files via upload

---
+++ value_of_pi.cpp
@@ -0,0 +1,27 @@
+// Exercise 10
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    double pi = 0;
+    long i;
+    long n;
+
+    cout << "Enter the value of n: ";
+    cin >> n;
+    cout << endl;
+
+    for (i = 0; i < n; i++)
+    {
+    if (i % 2 == 0)
+        pi = pi + (1.0 / (2 * i + 1));
+    else
+        pi = pi - (1.0 / (2 * i + 1));
+    }
+
+    pi *= 4;
+
+    cout << "pi = " << fixed << setprecision(10) << pi << endl;
+    return 0;
+}
