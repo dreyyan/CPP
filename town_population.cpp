82e4a89685f7971fa6f020f4f6e02c2141f1932a
dreyyan <adriandominic.tan@wvsu.edu.ph>
2024-11-15 00:52:17 +0800

    Add files via upload

---
+++ town_population.cpp
@@ -0,0 +1,40 @@
+// Exercise 7
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Declare Variables
+    int townAPopulation, townBPopulation, years = 0;
+    double townAGrowthRate, townBGrowthRate;
+
+    // Population of Town A
+    cout << "Population of Town A: ";
+    cin >> townAPopulation;
+
+    // Growth Rate of Town A
+    cout << "Growth Rate of Town A: ";
+    cin >> townAGrowthRate;
+
+    // Population of Town B
+    cout << "Population of Town B: ";
+    cin >> townBPopulation;
+
+    // Growth Rate of Town B
+    cout << "Growth Rate of Town B: ";
+    cin >> townBGrowthRate;
+
+    while (true) {
+        townAPopulation += (townAPopulation * townAGrowthRate);
+        townBPopulation += (townBPopulation * townBGrowthRate);
+        years++;
+        if (townAPopulation >= townBPopulation) {
+            break;
+        }
+    }
+
+    cout << "Population of Town A will be greater than or equal to Town B in " <<  years << " year/s." << endl;
+    cout << "\nTown Populations in " << years << " year/s:" << endl;
+    cout << "Town A Population: " << townAPopulation << endl;
+    cout << "Town B Population: " << townBPopulation << endl;
+    return 0;
+}
