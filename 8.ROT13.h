92f08de81b47c91570680832f1abec251d8478ea
dreyyan <adriandominic.tan@wvsu.edu.ph>
2024-11-17 01:06:51 +0800

    Add files via upload

---
+++ 8.ROT13.h
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cctype>
+using namespace std;
+
+void ROT13() {
+    string fileName, line, code;
+    cin >> fileName;
+
+    ifstream readFile("files\\" + fileName + ".rot13");
+    if (readFile.is_open()) {
+        while (getline(readFile, line)) {
+            code += line;
+        }
+        readFile.close();
+
+        for (char character : code) {
+            if (isalpha(character)) {
+                    char offset = isupper(character) ? 'A' : 'a';
+                    character = (character - offset + 13) % 26 + offset;
+                    cout << character;
+            }
+        }
+    } else {
+        cout << "Inaccessible file.";
+    }
+
+}
