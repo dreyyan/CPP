6e3c89503fc302981cde9b52666aa28cd994a4fe
dreyyan <adriandominic.tan@wvsu.edu.ph>
2024-11-19 18:15:52 +0800

    Add files via upload

---
+++ dequeReversal.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <deque>
+using namespace std;
+
+int main() {
+    deque<int> dq = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int firstElement, lastElement;
+    // Reverse Deque
+    reverse(dq.begin(), dq.end());
+    // Display Deque
+    for (size_t num : dq) {
+        cout << num << ' ';
+    }
+    return 0;
+}
