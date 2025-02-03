198bad6160d457d003a0eda8eb9ca0359d4dcf2d
dreyyan <adriandominic.tan@wvsu.edu.ph>
2025-02-03 10:18:07 +0800

    Update 1.printTodaysDate.h

--- 1.printTodaysDate.h
+++ 1.printTodaysDate.h
@@ -1,11 +1,12 @@
 // 1. Print Today's Date
 #include <iostream>
 #include <ctime>
+#include <iomanip>
 using namespace std;
 
-void printTodaysDate() {
+int main() {
     time_t seconds = time(0);
     struct tm datetime = *localtime(&seconds);
-
-    cout << "Date: " << datetime.tm_mon - 1 << "/" << datetime.tm_mday << "/" << datetime.tm_year + 1900;
+    cout << "Date: " << setw(2) << setfill('0') << datetime.tm_mon + 1 << " / " << setw(2) << setfill('0') << datetime.tm_mday << " / " << datetime.tm_year + 1900;
+    return 0;
 }
