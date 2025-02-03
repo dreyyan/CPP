00fa4b455d08a3f6798bf2c6a9a16927d6f8d2b1
dreyyan <adriandominic.tan@wvsu.edu.ph>
2025-02-03 10:15:05 +0800

    Update 1.printTodaysDate.h

--- 1.printTodaysDate.h
+++ 1.printTodaysDate.h
@@ -4,7 +4,7 @@
 using namespace std;
 
 void printTodaysDate() {
-    time_t seconds = time(NULL);
+    time_t seconds = time(0);
     struct tm datetime = *localtime(&seconds);
 
     cout << "Date: " << datetime.tm_mon - 1 << "/" << datetime.tm_mday << "/" << datetime.tm_year + 1900;
