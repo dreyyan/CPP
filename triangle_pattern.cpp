01d66945ed9b4c7ea9b2466d4dd4bdcbfc9dedc0
dreyyan <adriandominic.tan@wvsu.edu.ph>
2025-02-05 21:07:47 +0800

    2

--- triangle_pattern.cpp
+++ triangle_pattern.cpp
@@ -2,17 +2,23 @@
 using namespace std;
 
 int main() {
-    int columns, rows = 0;
+    // Prompt User to Enter # of Columns
+    int num_of_columns;
+    while (true) {
+        cout << "Enter # of Columns: ";
+        cin >> num_of_columns;
 
-    cout << "Enter # of Columns: ";
-    cin >> columns;
+        // Error Message
+        if (num_of_columns < 1) {
+            cerr << "[ Invalid Input | Number of columns must be at least 1 ]\n";
+        } else { break; }
+    }
 
-    for (int i = 0; i < columns; i++) {
-
-        for (int j = 0; j <= i; j++) {
-                cout << "*";
-        }
-        cout << "\n";
+    // Display Triangle
+    for (size_t i = 1; i <= num_of_columns; ++i) {
+        for (size_t j = 0; j < i; ++j) {
+            cout << '*';
+        } cout << '\n';
     }
 
     return 0;
