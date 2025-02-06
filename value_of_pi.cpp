7318b2aebe8c84eda3ace7d9a53212977daee972
dreyyan <adriandominic.tan@wvsu.edu.ph>
2025-02-06 14:57:56 +0800

    2

--- value_of_pi.cpp
+++ value_of_pi.cpp
@@ -2,26 +2,38 @@
 #include <iostream>
 #include <iomanip>
 using namespace std;
-int main()
-{
+
+int main() {
+    int n;
     double pi = 0;
-    long i;
-    long n;
 
-    cout << "Enter the value of n: ";
-    cin >> n;
-    cout << endl;
+    // Prompt User to Enter # of Terms
+    while (true) {
+        cout << "Enter Value of n: ";
+        cin >> n;
 
-    for (i = 0; i < n; i++)
-    {
-    if (i % 2 == 0)
-        pi = pi + (1.0 / (2 * i + 1));
-    else
-        pi = pi - (1.0 / (2 * i + 1));
+        if (cin.fail() || n <= 0) {
+            cin.clear();
+            cin.ignore(50, '\n');
+            cerr << "[ ERROR | Invalid Number ]\n";
+        } else { break; }
     }
 
+    // Solve for the Approximation of the Value of Pi [Leibniz Formula]
+    for (size_t i = 0; i < n; ++i) {
+        if (i % 2 == 0) {
+            pi += (1.0 / (2 * i + 1));
+        } else {
+            pi -= (1.0 / (2 * i + 1));
+        }
+    }
+
+    // Multiply Pi by 4 for Correct Approximation
     pi *= 4;
 
-    cout << "pi = " << fixed << setprecision(10) << pi << endl;
+    // Display the Result
+    cout << fixed << setprecision(10);
+    cout << "Value of Pi: " << pi << '\n';
+
     return 0;
 }
