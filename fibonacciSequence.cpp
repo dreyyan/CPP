434b369f44007de6a5c6fcada232ca94f8c590db
dreyyan <adriandominic.tan@wvsu.edu.ph>
2025-02-07 19:02:15 +0800

    2

--- fibonacciSequence.cpp
+++ fibonacciSequence.cpp
@@ -9,27 +9,34 @@ void sum_of_fibonacci_sequence() {
         cout << "# of Terms: ";
         cin >> num_of_terms;
 
+        // ERROR: Invalid Number
         if (cin.fail()) {
             cin.clear();
             cin.ignore(100, '\n');
             cerr << "[ ERROR | Invalid Number ]\n";
-        } else if (num_of_terms < 2) {
+        } else if (num_of_terms < 2) { // ERROR: # of Terms < 2
             cerr << "[ ERROR | # of terms must be greater than 2 ]\n";
         } else { break; }
     }
 
+    // Display Sum of 1st & 2nd Term
+    cout << num1 << " + " << num2;
     total_sum = num1 + num2;
 
-    // Solve for Sum of Fibonacci Numbers
+    // Solve for Sum of Fibonacci Numbers & Display
     for (size_t i = 2; i < num_of_terms; ++i) {
         sum = num1 + num2;
         total_sum += sum;
+
+        if (i != num_of_terms) cout << " + " << sum;
+        else cout << sum << " = ";
+
         num1 = num2;
         num2 = sum;
     }
 
     // Display Sum of Fibonacci Numbers
-    cout << "Sum = " << total_sum << '\n';
+    cout << " = " << total_sum << '\n';
 }
 
 int main() {
