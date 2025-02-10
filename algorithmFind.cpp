6aedd0953748d1971a212edf906f6cb3b2b71482
dreyyan <adriandominic.tan@wvsu.edu.ph>
2025-02-11 00:31:36 +0800

    2

--- algorithmFind.cpp
+++ algorithmFind.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 #include <algorithm>
-#include <array>
+#include <vector>
 using namespace std;
+// find (linear search)
+void algorithm_find(vector<int> list, int n) {
+    auto it = find(list.begin(), list.end(), n);
+
+    if (it != list.end()) { // Number exists
+        cout << "Number '" << n << "' found at index " << distance(list.begin(), it) << '\n';
+    } else { // Number does not exist
+        cout << "Number '" << n << "' does not exist...\n";
+    }
+}
 
 int main() {
-    array<string, 3> words = {"ocean", "river", "sea"};
-    auto it = find(words.begin(), words.end(), "sea");
-    if (it != words.end()) {
-        cout << "Element '" << *it << "' found!";
-    } else {
-        cout << "Element not found.";
-    }
+    vector<int> numbers = {1, 2, 3, 4, 5};
+    algorithm_find(numbers, 3);
     return 0;
 }
