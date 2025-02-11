fb094568e7041bc3c5cf80bc803c83e792ac3994
dreyyan <adriandominic.tan@wvsu.edu.ph>
2025-02-11 20:23:25 +0800

    2

--- palindrome_check.cpp
+++ palindrome_check.cpp
@@ -1,25 +1,20 @@
 #include <iostream>
+#include <algorithm>
 #include <string>
 using namespace std;
+// Write a program that inputs a word and checks if it is a palindrome or not
+bool is_palindrome(string word) {
+    string reversed_word = word;
+    reverse(reversed_word.begin(), reversed_word.end());
+    // Check if word and reversed word are equal
+    if (word == reversed_word) return true; else return false;
+}
 
 int main() {
     string word;
-    string reverseWord;
-
-    cout << "Enter word:";
-    cin >> word;
-
-    for (int i = word.length() - 1; i >= 0; i--) {
-        reverseWord += word[i];
-    }
-
-    if (reverseWord == word) {
-        cout << word << " is a palindrome.";
-    }
-
-    else {
-        cout << word << " is not a palindrome.";
-    }
-
+    // Prompt user to enter a word
+    cout << "Enter a word: ";
+    getline(cin, word);
+    cout << word << " is " << (is_palindrome(word) ? "" : "not ") << "a palindrome.";
     return 0;
 }
