5154dfe0e3fef6c9e88502fa03aa2adc4977ff83
dreyyan <adriandominic.tan@wvsu.edu.ph>
2025-02-12 21:40:00 +0800

    2

---
+++ RI.separate_even_odd.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void arraySeparateEvenOdd(int array[], int arraySize) {
+
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int arrSize = sizeof(arr) / sizeof(arr[0]);
+    arraySeparateEvenOdd(arr, arrSize);
+    return 0;
+}
--- RI.vowel_counter.cpp
+++ RI.vowel_counter.cpp
@@ -1,24 +1,32 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
-int main() {
-    string stringInput;
-    char vowels[] = {'a', 'e', 'i', 'o', 'u'};
-    int sizeVowels = sizeof(vowels) / sizeof(vowels[0]);
-    int vowelCounter = 0;
-
-    cout << "Enter Series of Letters: ";
-    cin >> stringInput;
-
-    for (int i = 0; i < stringInput.length(); i++) {
-        for (int j = 0; j < sizeVowels; j++) {
-            if (stringInput[i] == vowels[j]) {
-                vowelCounter += 1;
+int vowel_counter(const vector<char> vowel_list, string word) {
+    size_t counter = 0;
+    for (const char& letter : word) {
+        for (const char& vowel : vowel_list) {
+            if (vowel == letter) {
+                ++counter; break;
             }
         }
     }
+    return counter;
+}
 
-    cout << "\nNumber of Vowels: " << vowelCounter;
+int main() {
+    vector<char> vowels = {'a', 'a', 'e', 'i', 'o', 'u'};
+    string word;
+    int vowel_count;
+
+    // Prompt user to enter a word
+    cout << "Enter a word: ";
+    getline(cin, word);
+
+    vowel_count = vowel_counter(vowels, word);
+    if (vowel_count) {
+        cout << "There are " << vowel_count << " vowel/s in the word " << word << ".\n";
+    } else cout << "There are no vowels in the word " << word << '\n';
     return 0;
 }
