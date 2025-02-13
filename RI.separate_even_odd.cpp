0caf66506bdabf0279e524adddad77bcaf42a8f6
dreyyan <adriandominic.tan@wvsu.edu.ph>
2025-02-13 19:03:29 +0800

    2

--- RI.separate_even_odd.cpp
+++ RI.separate_even_odd.cpp
@@ -1,13 +1,31 @@
+#include <algorithm>
 #include <iostream>
+#include <vector>
 using namespace std;
-
-void arraySeparateEvenOdd(int array[], int arraySize) {
-
+// Write a program that separates even and odd numbers
+void separate_even_odd(vector<int> numbers) {
+    vector<int> even, odd;
+    for (const int& number : numbers) {
+        if (number % 2 == 0) {
+            even.push_back(number);
+        } else {
+            odd.push_back(number);
+        }
+    }
+    // Display even numbers
+    cout << "Even Numbers: ";
+    for (const int& number : even) {
+        cout << number << ' ';
+    } cout << '\n';
+    // Display odd numbers
+    cout << "Odd Numbers: ";
+    for (const int& number : odd) {
+        cout << number << ' ';
+    } cout << '\n';
 }
 
 int main() {
-    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int arrSize = sizeof(arr) / sizeof(arr[0]);
-    arraySeparateEvenOdd(arr, arrSize);
+    vector<int> numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    separate_even_odd(numbers);
     return 0;
 }
