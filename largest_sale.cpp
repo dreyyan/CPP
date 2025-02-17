54e418168672c5b1c18afb9ec85372fb9ab32faf
dreyyan <adriandominic.tan@wvsu.edu.ph>
2025-02-17 16:17:12 +0800

    2

--- largest_sale.cpp
+++ largest_sale.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 // CC 203 Seatwork:
 // Declare & initialize an array of 10 sales & determine the largest sale amount from the given sales
-void display_largest_sale(const double sales[], size_t size) {
+void display_largest_sale(const double sales[], int size) {
     double largest_sale = sales[0]; // Initialize variable to the first sale
     // Loop: Determine largest sale by comparison of values
     for (size_t i = 1; i < size; ++i) {
@@ -16,7 +16,7 @@ void display_largest_sale(const double sales[], size_t size) {
 
 int main() {
     double sales[10] = {12.50, 8.35, 19.60, 25.00, 14.00, 39.43, 35.90, 98.23, 66.65, 35.64}; // Initialize array for 10 sales
-    size_t size = sizeof(sales) / sizeof(sales[0]); // Raw Implementation: Determine size of array
+    int size = sizeof(sales) / sizeof(sales[0]); // Raw Implementation: Determine size of array
     display_largest_sale(sales, size); // Invoke function
     return 0;
 }
